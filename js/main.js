b52b719168438d52023428bfcd8a54cc2055080a
Victoria <vicglee1234007@gmail.com>
2020-11-07 01:30:02 -0600

    Added svg references into legend, altered some svg styling, erased console.logs, aligned prev & next slideshow btns

--- js/main.js
+++ js/main.js
@@ -1,31 +1,4 @@
-// function slideshow(){
-//   let i = 0;
-//   let images = [];
-//   let time = 3000;
-
-//   //images
-//   images[0] = "images/bb-slideshow-example-1.jpg";
-//   images[1] = "images/bb-slideshow-example-2.jpg";
-//   images[2] = "images/bb-slideshow-example-3.jpg";
-
-//   function changeSlide(){
-//     document.slide.src = images[i];
-
-//     if(i<images.length-1){
-//       i++;
-//     }else{
-//       i=0;
-//     }
-//   }
-
-//   setTimeout("changeSlide()", time);
-// }
-
-
-// window.onload = changeSlide;
-// <img name="slide" width = x, height = y>
-
-let counter = 1;
+var counter = 1;
 const slide = document.querySelector(".slideshow-container");
 const slideImg = document.querySelectorAll(`.slideshow-container img`);
 
@@ -34,24 +7,38 @@ const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
 const size = slideImg[0].clientWidth||940;
-console.log("img", slideImg, "size", size);
 
 slide.style.transform = `translateX(-940px)`;
+let move = size*counter;
 
 //Btn listeners
-
 nextBtn.addEventListener("click", ()=>{
+  //if condition prevents clicking pass possible slides
+  if(counter>=slideImg.length-1) return;
   slide.style.transition = "transform 0.4s ease-in-out";
   counter++;
-  console.log("Counter", counter);
-  let move = size*counter;
-  slide.style.transform = `translateX(-${move}px)`;
+  slide.style.transform = `translateX(-${size*counter}px)`;
 })
 
 prevBtn.addEventListener("click", ()=>{
+  if(counter<=0) return;
   slide.style.transition = "transform 0.4s ease-in-out";
   counter--;
-  console.log("Counter", counter);
-  let move = size*counter;
-  slide.style.transform = `translateX(-${move}px)`;
+  slide.style.transform = `translateX(-${size*counter}px)`;
+})
+
+//Also prevents slideshow from iterating passed possible slides
+slide.addEventListener("transitionend", ()=> {
+
+  if(slideImg[counter].id==="lastClone"){
+    slide.style.transition = "none";
+    counter = slideImg.length-2;
+    slide.style.transform = `translateX(-${size*counter}px)`;
+  }
+
+  if(slideImg[counter].id==="firstClone"){
+    slide.style.transition = "none";
+    counter = slideImg.length-counter;
+    slide.style.transform = `translateX(-${size*counter}px)`;
+  }
 })
