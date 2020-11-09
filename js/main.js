f2e4dd1d9b6457a86f7e0a691d007bc1320f1ae6
Victoria <vicglee1234007@gmail.com>
2020-11-08 21:26:25 -0600

    Adjusted slideshow timing when switching window sizes

--- js/main.js
+++ js/main.js
@@ -1,84 +1,90 @@
 //Slideshow functionality
+const slideshow = document.querySelector(".slideshow");
+console.log("window", window)
 
-var counter = 1;
-let clicked = false;
-const slide = document.querySelector(".slideshow-container");
-const slideImg = document.querySelectorAll(`.slideshow-container img`);
+function slideshowInit(){
+  let timerId;
 
-//Btns
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
+  var counter = 1;
+  const slide = document.querySelector(".slideshow-container");
+  const slideImg = document.querySelectorAll(`.slideshow-container img`);
 
-const size = slideImg[0].clientWidth||940;
+  //Btns
+  const prevBtn = document.querySelector("#prevBtn");
+  const nextBtn = document.querySelector("#nextBtn");
 
-slide.style.transform = `translateX(-940px)`;
-let move = size*counter;
+  const size = slideImg[0].clientWidth||940;
 
-function nextSlide(){
-  if(counter>=slideImg.length-1) return;
-  clicked = true;
-  slide.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  slide.style.transform = `translateX(-${size*counter}px)`;
-  clicked = false;
-}
+  slide.style.transform = `translateX(-940px)`;
 
-//Btn listeners
-nextBtn.addEventListener("click", nextSlide)
+  function nextSlide(){
+    if(getComputedStyle(slideshow).display!=="none"){
+      if(counter>=slideImg.length-1) return;
+      console.log("NEXT SLIDE")
+      slide.style.transition = "transform 0.4s ease-in-out";
+      counter++;
+      slide.style.transform = `translateX(-${size*counter}px)`;
+    }
+  }
 
-prevBtn.addEventListener("click", ()=>{
-  if(counter<=0) return;
-  clicked = true;
+  //Btn listeners
+  nextBtn.addEventListener("click", nextSlide)
+
+  prevBtn.addEventListener("click", ()=>{
+    if(counter<=0) return;
   slide.style.transition = "transform 0.4s ease-in-out";
   counter--;
   slide.style.transform = `translateX(-${size*counter}px)`;
-  clicked = false;
-})
+  })
 
-//Also prevents slideshow from iterating passed possible slides
-slide.addEventListener("transitionend", ()=> {
-  if(slideImg[counter].id==="lastClone"){
-    slide.style.transition = "none";
-    counter = slideImg.length-2;
-    slide.style.transform = `translateX(-${size*counter}px)`;
-  }
+  //Also prevents slideshow from iterating passed possible slides
+  slide.addEventListener("transitionend", ()=> {
+    if(slideImg[counter].id==="lastClone"){
+      slide.style.transition = "none";
+      counter = slideImg.length-2;
+      slide.style.transform = `translateX(-${size*counter}px)`;
+    }
 
-  if(slideImg[counter].id==="firstClone"){
-    slide.style.transition = "none";
-    counter = slideImg.length-counter;
-    slide.style.transform = `translateX(-${size*counter}px)`;
-  }
-})
+    if(slideImg[counter].id==="firstClone"){
+      slide.style.transition = "none";
+      counter = slideImg.length-counter;
+      slide.style.transform = `translateX(-${size*counter}px)`;
+    }
+  })
 
 
-//Slideshow timer
-const IntervalTimer = function (callback, delay){
-  let timerId;
-  let state = 1; // 1 = running, 2 = paused;
+  //Slideshow timer
+  const IntervalTimer = function (callback, delay){
+    let state = 1; // 1 = running, 2 = paused;
 
-  this.pause = function(){
-    if(state !== 1) return;
-    window.clearInterval(timerId);
-    state = 2;
-    console.log("pause", state)
-  }
+    this.pause = function(){
+      if(state !== 1) return;
+      window.clearInterval(timerId);
+      state = 2;
+      console.log("pause", state)
+    }
 
-  this.resume = function(){
-    if(state!==2) return;
+    this.resume = function(){
+      if(state!==2) return;
+      timerId = window.setInterval(callback, delay)
+      console.log("resume", state)
+      state = 1;
+    }
+
     timerId = window.setInterval(callback, delay)
-    console.log("resume", state)
     state = 1;
+    console.log(state, "State")
   }
 
-  timerId = window.setInterval(callback, delay)
-  state = 1;
-  console.log(state, "State")
+  const timer = new IntervalTimer(nextSlide, 3000);
+
+  //mouseover detection
+  slideshow.addEventListener("mouseenter", timer.pause)
+  slideshow.addEventListener("mouseleave", timer.resume)
+
 }
+window.addEventListener("load", slideshowInit);
 
-const timer = new IntervalTimer(nextSlide, 3000);
-const slideshow = document.querySelector(".slideshow")
 
-//mouseover detection
-slideshow.addEventListener("mouseenter", timer.pause)
-slideshow.addEventListener("mouseleave", timer.resume)
+
 
